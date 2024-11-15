5420528ad4bc0fd2c32920e3cfc153efeb1af7ff
ShaYri12 <sharjeelb60@gmail.com>
2024-11-15 20:10:26 +0500

    fixing the deployment

--- i18n.js
+++ i18n.js
@@ -11,14 +11,22 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
+    supportedLngs: ["en", "pt-BR", "ru"], // Add your supported languages here
     debug: process.env.NODE_ENV === "development",
 
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // React already handles escaping
     },
 
     backend: {
+      // Updated loadPath to fetch from nested folder structure
       loadPath: "/locales/{{lng}}.json",
+    },
+
+    // Language detection setup
+    detection: {
+      order: ["localStorage", "path", "navigator"],
+      caches: ["localStorage"], // Store the selected language in localStorage
     },
 
     lng:
